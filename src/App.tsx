7501dea02946951201af8f809772b3001530b9b8
kathecr <kcastelblancor@gmail.com>
2021-12-31 11:21:47 -0500

    🚧 Integreted react router success

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import NotFound from './pages/NotFound'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
-const Layout = (children: React.ReactNode) => (
+const Layout = () => (
   <div className="root">
     <Header />
-    {children}
+    <Outlet />
     <Footer />
   </div>
 );
